a99aaa731ae5d7ac7386ff19167a756c633bb984
neowho <48194907+neowho@users.noreply.github.com>
2022-11-28 02:11:26 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -375,7 +375,7 @@ View_User_info(){
 # 设置 配置信息
 Set_config_user(){
 	echo "请输入要设置的用户 用户名(请勿重复, 用于区分, 不支持中文、空格, 会报错 !)"
-	read -e -p "(默认: doubi):" ssr_user
+	read -e -p "(默认用户名: neovip):" ssr_user
 	[[ -z "${ssr_user}" ]] && ssr_user="doubi"
 	ssr_user=$(echo "${ssr_user}"|sed 's/ //g')
 	echo && echo ${Separator_1} && echo -e "	用户名 : ${Green_font_prefix}${ssr_user}${Font_color_suffix}" && echo ${Separator_1} && echo
